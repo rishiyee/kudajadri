3d737f45e02196bf70e8a72757fac066838f1911
Hrishi <145846048+rishiyee@users.noreply.github.com>
2025-05-29 22:31:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Booking from "./components/Booking/Booking";
